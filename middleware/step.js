e23cf35485242ba4187c07109253a676bb99d2f6
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-16 09:49:11 +0900

    リダイレクト設定

---
+++ middleware/step.js
@@ -0,0 +1,10 @@
+export default ({ from, route, redirect, }) => {
+  if (
+    route.name === 'step1' ||
+    (from?.name === 'step1' && route.name === 'step2') ||
+    (from?.name === 'step2' && route.name === 'step1') ||
+    (from?.name === 'step2' && route.name === 'step3') ||
+    (from?.name === 'step3' && route.name === 'step2')
+  ) return;
+  redirect('/step1');
+}
