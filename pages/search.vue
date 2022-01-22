c0049b1f1866d42e0347cae88c4374179c30ac62
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-22 20:41:45 +0900

    検索ページ

---
+++ pages/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="title">検索</h1>
+    <Pagination />
+  </div>
+</template>
+
+<script>
+import Pagination from '@/components/Pagination.vue'
+
+export default {
+  name: 'Search',
+
+  components: {
+    Pagination,
+  },
+}
+</script>
