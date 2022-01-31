835e3584bb6c6ccf4a5a580cf8ed6134a36ce49b
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-02-01 02:34:00 +0900

    タブの挙動作成

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="tabs" :class="tabsClass">
-    <ul>
+  <div ref="tabs" class="tabs" :class="tabsClass">
+    <ul ref="items">
       <li
         v-for="item in items"
         :key="item.id"
-        :class="{ 'is-active': item.id === currentId }"
+        :class="{ 'is-active': item.id === activeId }"
       >
         <a href="#" @click.prevent="onClickTab(item)">{{ item.label }}</a>
       </li>
@@ -13,10 +13,20 @@
 </template>
 
 <script>
+import { gsap } from 'gsap'
+
 export default {
   name: 'Tabs',
 
   props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    activeId: {
+      type: String,
+      default: '',
+    },
     boxed: {
       type: Boolean,
       default: false,
@@ -25,45 +35,6 @@ export default {
 
   data() {
     return {
-      items: [
-        {
-          id: 'news',
-          label: 'ニュース',
-        },
-        {
-          id: 'economy',
-          label: '経済',
-        },
-        {
-          id: 'entertainment',
-          label: 'エンタメ',
-        },
-        {
-          id: 'sports',
-          label: 'スポーツ',
-        },
-        {
-          id: 'domestic',
-          label: '国内',
-        },
-        {
-          id: 'international',
-          label: '国際',
-        },
-        {
-          id: 'it',
-          label: 'IT',
-        },
-        {
-          id: 'science',
-          label: '科学',
-        },
-        {
-          id: 'region',
-          label: '地域',
-        },
-      ],
-      currentId: 'news',
     }
   },
 
@@ -75,10 +46,53 @@ export default {
     },
   },
 
+  watch: {
+    activeId(newVal) {
+      if (!newVal) return
+      this.$nextTick(() => {
+        this.setActiveTabCenter()
+      })
+    },
+  },
+
   methods: {
+    setActiveTabCenter() {
+      // 各要素を取得
+      const tabsEl = this.$refs.tabs
+      const activeEl = this.$refs.items.querySelector('.is-active')
+      // 各要素の位置を取得
+      const tabsX = tabsEl.getBoundingClientRect().x
+      const activeX = activeEl.getBoundingClientRect().x
+      // 各要素の中心を取得
+      const tabsCenter = tabsEl.clientWidth / 2 + tabsX
+      const activeCenter = activeEl.clientWidth / 2 + activeX
+      // スクロール位置を移動
+      const scrollLeft = tabsEl.scrollLeft + activeCenter - tabsCenter
+      gsap.to(tabsEl, {
+        duration: 0.5,
+        scrollLeft,
+      })
+    },
     onClickTab(item) {
-      console.log(item)
+      this.$emit('clickTab', item.id)
     },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.tabs {
+  &::-webkit-scrollbar {
+    height: 8px;
+  }
+  &::-webkit-scrollbar-track {
+    background: $c_primary;
+    border-radius: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: $c_border_light;
+    border-radius: 4px;
+    border: 1px solid $c_primary;
+  }
+}
+</style>
--- pages/news.vue
+++ pages/news.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Tabs boxed />
+    <Tabs :items="items" :active-id="activeId" boxed @clickTab="clickTab" />
   </div>
 </template>
 
@@ -12,6 +12,60 @@ export default {
 
   components: {
     Tabs,
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          id: 'news',
+          label: 'ニュース',
+        },
+        {
+          id: 'economy',
+          label: '経済',
+        },
+        {
+          id: 'entertainment',
+          label: 'エンタメ',
+        },
+        {
+          id: 'sports',
+          label: 'スポーツ',
+        },
+        {
+          id: 'domestic',
+          label: '国内',
+        },
+        {
+          id: 'international',
+          label: '国際',
+        },
+        {
+          id: 'it',
+          label: 'IT',
+        },
+        {
+          id: 'science',
+          label: '科学',
+        },
+        {
+          id: 'region',
+          label: '地域',
+        },
+      ],
+      activeId: '',
+    }
+  },
+
+  mounted() {
+    this.activeId = this.items[0].id
+  },
+
+  methods: {
+    clickTab(id) {
+      this.activeId = id;
+    },
   },
 }
 </script>
